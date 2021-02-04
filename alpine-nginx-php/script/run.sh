3b8aa1001116734a9e476385d409219bc277cf2f
chensixu <chensixu@jjys168.com>
2021-02-04 10:59:53 +0800

    docker alpine-php 1.0-dev

---
+++ alpine-nginx-php/script/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#ensure www-data user exists
+set -x \
+    && addgroup -g 82 -S www-data \
+    && adduser -u 82 -D -S -G www-data www-data
+mkdir -p /home
+mkdir -p /www
+chown www-data.www-data /home -R
