035a7ab1cc9d6a97ae14c17dbf602c36d4cada3d
chensixu <facelook@163.com>
2024-06-18 15:17:50 +0800

    repair win string ctrl CF

--- alpine-php/script/run.sh
+++ alpine-php/script/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #ensure www-data user exists
-set -x \
-    && addgroup -g 82 -S www-data
+set -x && addgroup -g 82 -S www-data
 adduser -u 82 -D -S -G www-data www-data
 mkdir -p /home
 mkdir -p /www
